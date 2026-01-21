0bb9adb448ebf2856a5b212aeead9c7c397f3924
Samioul51 <akmsamioulislam51@gmail.com>
2026-01-21 12:29:55 +0600

    Database Connection Established

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import connectDB from './db/dbConnect.js';
 
 const app=express();
 
 dotenv.config();
 
 const port=process.env.PORT || 3000;
+
+// MongoDB Connection
+
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("Server Running");
