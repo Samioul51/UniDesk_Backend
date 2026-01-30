748685a1b3794ecffa03fb2017027c0078ef297b
Samioul51 <akmsamioulislam51@gmail.com>
2026-01-30 19:00:15 +0600

    feat: All users information api created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createUser } from "../controllers/UserController/user.controller.js";
+import { createUser, getUsers } from "../controllers/UserController/user.controller.js";
 
 
 const router=express.Router();
 
 router.post("/users",createUser);
+
+router.get("/users",getUsers);
 
 export default router;
