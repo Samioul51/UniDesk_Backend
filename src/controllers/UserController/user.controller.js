1e1ae106c886f016665c09d39ecb7d4d1164f7b4
Samioul51 <akmsamioulislam51@gmail.com>
2026-01-30 19:08:55 +0600

    feat: single user info retrieval api implemented

--- src/controllers/UserController/user.controller.js
+++ src/controllers/UserController/user.controller.js
@@ -2,30 +2,47 @@ import { User } from "../../models/UserModel/user.model.js";
 
 // User creation
 
-export const createUser=async(req,res)=>{
-    try{
-        const newUser=req.body;
+export const createUser = async (req, res) => {
+    try {
+        const newUser = req.body;
 
-        const existingUser=await User.findOne({email:newUser.email});
+        const existingUser = await User.findOne({ email: newUser.email });
 
-        if(existingUser)
+        if (existingUser)
             return res.status(200).json(existingUser);
 
-        const user=await User.create(newUser);
+        const user = await User.create(newUser);
 
-        res.status(201).json(user);
+        return res.status(201).json(user);
     }
-    catch(error){
-        res.status(400).json({message:error.message});
+    catch (error) {
+        return res.status(400).json({ message: error.message });
     }
 };
 
-export const getUsers=async(req,res)=>{
-    try{
-        const users=await User.find();
-        res.status(200).json(users);
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json(users);
     }
-    catch(error){
-        res.status(400).json({message:error.message});
+    catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+};
+
+export const getSingleUser = async (req, res) => {
+    try {
+        const email = req.params.email;
+        const user = await User.findOne({ email });
+
+        if (!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
     }
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, getUsers } from "../controllers/UserController/user.controller.js";
+import { createUser, getSingleUser, getUsers } from "../controllers/UserController/user.controller.js";
 
 
 const router=express.Router();
@@ -7,5 +7,7 @@ const router=express.Router();
 router.post("/users",createUser);
 
 router.get("/users",getUsers);
+
+router.get("/users/:email",getSingleUser);
 
 export default router;
