7b2a891d4c93d96c86ec649a8ffcf9f546cb3d18
Samioul51 <akmsamioulislam51@gmail.com>
2026-02-11 14:57:53 +0600

    Repository model implemented

---
+++ src/models/RepositoryModel/repository.model.js
@@ -0,0 +1,74 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+
+const repositorySchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    courseCode: {
+        type: String,
+        required: true,
+        uppercase: true,
+        index: true
+    },
+    courseName: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    year: {
+        type: String,
+        required: true
+    },
+    semester: {
+        type: String,
+        required: true
+    },
+    itemType: {
+        type: String,
+        required: true,
+        enum: ["notes", "question bank", "solved questions", "assignment", "project_report", "lab_report", "other"]
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    uploader: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["pending", "approved", "rejected"],
+        default: "pending"
+    },
+    rejectedReason: {
+        type: String
+    },
+    downloadCount: {
+        type: Number,
+        default: 0
+    },
+    contributionPoints: {
+        type: Number,
+        default: 0
+    },
+    approvedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    approvedAt:{
+        type:Date
+    }
+
+}, {
+    timestamps: true
+});
+
+export const Repository = mongoose.model("repository", repositorySchema);
