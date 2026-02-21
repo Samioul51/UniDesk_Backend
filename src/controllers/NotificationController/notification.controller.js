68607541d4c40f3c2b457690973979accfc0396e
Samioul51 <akmsamioulislam51@gmail.com>
2026-02-21 15:03:49 +0600

    Mark notification as read APIs implemented

--- src/controllers/NotificationController/notification.controller.js
+++ src/controllers/NotificationController/notification.controller.js
@@ -4,7 +4,7 @@ import { Notification } from "../../models/NotificationModel/notification.model.
 
 export const getNotifications = async (req, res) => {
     try {
-        const {userID} = req.query;
+        const { userID } = req.query;
 
         if (!userID)
             return res.status(400).json({
@@ -27,22 +27,104 @@ export const getNotifications = async (req, res) => {
 
 // Unread count
 
-export const getUnreadCount=async(req,res)=>{
+export const getUnreadCount = async (req, res) => {
     try {
-        const {userID}=req.query;
+        const { userID } = req.query;
 
-        const count=await Notification.countDocuments({
-            receiver:userID,
-            isRead:false
+        if (!userID)
+            return res.status(400).json({
+                success: false,
+                message: "User ID required"
+            });
+
+        const count = await Notification.countDocuments({
+            receiver: userID,
+            isRead: false
         });
 
         return res.status(200).json({
             success: true,
-            unreadCount:count
+            unreadCount: count
         });
     } catch (error) {
         return res.status(500).json({
             message: error.message
         });
     }
-}
+};
+
+// Mark one as read
+
+export const markOneAsRead = async (req, res) => {
+    try {
+        const { userID } = req.body;
+
+        if (!userID)
+            return res.status(400).json({
+                success: false,
+                message: "User ID required"
+            });
+
+        const notification=await Notification.findOneAndUpdate(
+            {
+                _id: req.params.id,
+                receiver: userID
+            },
+            {
+                isRead:true
+            }
+        );
+
+        if(!notification)
+            return res.status(404).json({
+            success: true,
+            message:"Notification not found"
+        });
+
+
+        return res.status(200).json({
+            success: true,
+            message:"Notification marked as read"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+// Mark all as read
+
+export const markAllAsRead = async (req, res) => {
+    try {
+        const { userID } = req.body;
+
+        if (!userID)
+            return res.status(400).json({
+                success: false,
+                message: "User ID required"
+            });
+
+        await Notification.updateMany(
+            {
+                receiver: userID,
+                isRead:false
+            },
+            {
+                $set:{
+                    isRead:true
+                }
+            }
+        );
+
+        return res.status(200).json({
+            success: true,
+            message:"All notifications marked as read"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
--- src/utils/NotificationEngine/notificationService.js
+++ src/utils/NotificationEngine/notificationService.js
@@ -1,5 +1,5 @@
 import { Notification } from "../../models/NotificationModel/notification.model.js"
-import {io} from "../../index.js"
+import { io } from "../../index.js"
 
 export const notifyUsers = async ({
     receivers,
@@ -11,34 +11,37 @@ export const notifyUsers = async ({
     entityModel,
     redirectURL
 }) => {
+    try {
+        const notifications = receivers.map(userID => ({
+            receiver: userID,
+            sender,
+            type,
+            title,
+            message,
+            entityID,
+            entityModel,
+            redirectURL
+        }));
 
-    const notifications = receivers.map(userID => ({
-        receiver: userID,
-        sender,
-        type,
-        title,
-        message,
-        entityID,
-        entityModel,
-        redirectURL
-    }));
+        await Notification.insertMany(notifications);
 
-    await Notification.insertMany(notifications);
+        for (const userID of receivers) {
+            const old = await Notification.find({ receiver: userID }).sort({ createdAt: -1 }).skip(30);
 
-    for (const userID of receivers) {
-        const old = await Notification.find({ receiver: userID }).sort({ createdAt: -1 }).skip(30);
+            if (old.length > 0)
+                await Notification.deleteMany({
+                    _id: { $in: old.map(n => n._id) }
+                });
 
-        if (old.length > 0)
-            await Notification.deleteMany({
-                _id: { $in: old.map(n => n._id) }
+            const unreadCount = await Notification.countDocuments({
+                receiver: userID,
+                isRead: false
             });
 
-        const unreadCount = await Notification.countDocuments({
-            receiver: userID,
-            isRead: false
-        });
-
-        io.to(userID.toString()).emit("new_notification");
-        io.to(userID.toString()).emit("unread_count", unreadCount);
+            io.to(userID.toString()).emit("new_notification");
+            io.to(userID.toString()).emit("unread_count", unreadCount);
+        }
+    } catch (error) {
+        throw error;
     }
 };
