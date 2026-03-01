23061376d04690b58672da632aaf4cdd6cb14d17
Samioul51 <akmsamioulislam51@gmail.com>
2026-03-01 16:23:05 +0600

    Integrated authorization in appointment related APIs

--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,26 +1,28 @@
 import express from "express";
 import { bookAppointment, getAppointment, getFacultyAppointments, getStudentAppointments,updateAppointmentStatus } from "../controllers/AppointmentController/appointment.controller.js";
+import { verifyFirebaseToken } from "../middlewares/Auth/auth.middleware.js";
+import { verifyRole } from "../middlewares/Role/role.middleware.js";
 
 const router=express.Router();
 
 // Appointment creation
 
-router.post("/appointment",bookAppointment);
+router.post("/appointment",verifyFirebaseToken,verifyRole(["student"]),bookAppointment);
 
 // Get student appointments
 
-router.get("/appointment/student/:id",getStudentAppointments);
+router.get("/appointment/student/:id",verifyFirebaseToken,verifyRole(["student"]),getStudentAppointments);
 
 // Get faculty appointments
 
-router.get("/appointment/faculty/:id",getFacultyAppointments);
+router.get("/appointment/faculty/:id",verifyFirebaseToken,verifyRole(["faculty"]),getFacultyAppointments);
 
 // Appointment status update
 
-router.patch("/appointment/:id",updateAppointmentStatus);
+router.patch("/appointment/:id",verifyFirebaseToken,verifyRole(["student","faculty"]),updateAppointmentStatus);
 
 // Single appointment GET
 
-router.get("/appointment/:id",getAppointment);
+router.get("/appointment/:id",verifyFirebaseToken,verifyRole(["student","faculty"]),getAppointment);
 
 export default router;
